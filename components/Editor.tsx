f0ca86597cd2a0d20a5bc34642bb2a4d0f198b9e
nakada toyohisa <nakada@nuis.ac.jp>
2026-01-29 22:24:37 +0900

    iframeでAIが学習して重みを親画面に転送の途中

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -7,7 +7,8 @@ import { removeLineComments } from "@/lib/monaco-utils/removeLineComments";
 import { buildImportmap } from "@/lib/monaco-utils/buildImportmap";
 
 
-const buildIframeErrorHandlerScript = `
+// iframeに追加するエラー発生時に親ウィンドウにメッセージを送信するコード
+const BUILD_IFRAME_ERROR_HANDLER_SCRIPT = `
 <script>
     (function () {
         window.onerror = function (message, source, lineno, colno, error) {
@@ -22,6 +23,13 @@ const buildIframeErrorHandlerScript = `
     })();
 </script >`.replace(/[\r\n\t ]+/g, "");
 
+// スクリプトのみのEditorで書かれた文字からHTMLにするためのテンプレート
+const HTML_TEMPLATE = `
+<html>
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js"></script>
+    <script type="module">__CODE__</script>
+</html>`.replace(/[\r\n\t]+/g, "");
+
 interface EditorProps {
     onUpdateWeight: (weights: Record<string, number>) => void;
 }
@@ -31,7 +39,7 @@ export default function App({ onUpdateWeight }: EditorProps) {
     const workerRef = useRef<Worker | null>(null);
     const workerTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-    function ImportImportmap(htmlString: string, files: Record<string, string>): string {
+    function injectImportmap(htmlString: string, files: Record<string, string>): string {
         // '//'のコメントをとる
         const filesNoComm = Object.entries(files).reduce((a, e) =>
             ({ ...a, [e[0]]: removeLineComments(e[1]) }), {} as Record<string, string>);
@@ -60,11 +68,11 @@ export default function App({ onUpdateWeight }: EditorProps) {
 
             // 外部のjsファイルをimportmapで取り込む。この例の場合、import {testtemp01234} from 'test.js'; で使用する。
             const files = { 'test.js': `export const testtemp01234=0;` };
-            const htmlString = ImportImportmap(`<html><script type="module">${editorRef.current?.getValue()}</script></html>`, files);
+            const htmlString = injectImportmap(HTML_TEMPLATE.replace('__CODE__', editorRef.current?.getValue() || ""), files);
             const { html: inlined_html, insertions } = inlineHTML(htmlString, files);
-            const htmlStringWithErrorHandler = inlined_html.replace(/(<html[^>]*>)/i, `$1${buildIframeErrorHandlerScript}`);
+            const htmlStringWithErrorHandler = inlined_html.replace(/(<html[^>]*>)/i, `$1${BUILD_IFRAME_ERROR_HANDLER_SCRIPT}`);
 
-            // console.log(htmlStringWithErrorHandler)
+            console.log(htmlStringWithErrorHandler)
             editor_output_elem!.srcdoc = htmlStringWithErrorHandler;
 
         });
@@ -113,28 +121,59 @@ export default function App({ onUpdateWeight }: EditorProps) {
                     },
                 }}
                 defaultValue={`
-/*
-async function setup() {
-  const data = {
-    "原点通る直線": [[-1, -1], [0, 0], [1, 1], [2, 2],],
-    "原点通る折れ線": [[-1, -2], [0, 0], [1, 2], [2, 1],],
-    "原点通らない直線": [[-1, -2], [0, -1], [1, 0], [2, 1],],
-    "原点通らない折れ線": [[-1, -3], [0, -1], [1, 1], [2, 0],],
+async function getDataset(data) {
+  if (data === undefined) {
+    return {};
+  }
+  const series = {
+    x: data.map((e) => e[0]),
+    y: data.map((e) => e[1]),
   };
-
-  const { values, ranges, tensors } = await getDataset(
-    data?.["原点通らない折れ線"],
-  );
-  updateScatterplot({ values: [[], values], ranges });
-  const model = createModel({
-    units: 2,
-    useBias: true,
-    LearningRate: 0.05,
-  });
-  setupVisor({ onStart: async () => await learn({ epochs: 50, model, values, ranges, tensors }), });
+  return {
+    values: data.map((e) => ({ x: e[0], y: e[1] })),
+    ranges: {
+      x: [Math.min(...series.x) - 0.5, Math.max(...series.x) + 0.5],
+      y: [Math.min(...series.y) - 2.5, Math.max(...series.y) + 2.5],
+    },
+    tensors: {
+      x: tf.tensor2d(series.x, [data.length, 1]),
+      y: tf.tensor2d(series.y, [data.length, 1]),
+    },
+  };
 }
-document.addEventListener("DOMContentLoaded", setup);
-*/`}
+const { values, ranges, tensors } = await getDataset([[-1, -3], [0, -1], [1, 1], [2, 0],]);
+const [units,useBias,LearningRate,epochs] = [2,true,0.05,50];
+const model = tf.sequential();
+model.add(tf.layers.dense({
+    inputShape:[1], activation:"tanh",
+    units,useBias,
+}),);
+model.add(tf.layers.dense({ units: 1, useBias: false }));
+model.compile({
+    optimizer: tf.train.adam(LearningRate),
+    loss: tf.losses.meanSquaredError,
+    metrics: ["mse"],
+});
+const history = await model.fit(tensors.x,tensors.y,{
+    batchSize: tensors.x.shape[0],shuffle:true,
+    epochs,
+});
+const weights = {
+    w1: model.layers[0].getWeights()[0].dataSync(),
+    b: model.layers[0].getWeights()[1]?.dataSync(),
+    w2: model.layers[1].getWeights()[0].dataSync(),
+};
+const weightValues = {
+    wIn1: weights.w1[0],
+    wIn2: weights.w1[1] || 0,
+    wOut1: weights.w2[0],
+    wOut2: weights.w2[1] || 0,
+    b1: weights.b[0],
+    b2: weights.b[1] || 0,
+}
+console.log(values);
+window.parent.postMessage({type:'weights',values:weightValues});
+`}
             />
             <button className="px-3 py-1 text-xs font-semibold text-gray-200 bg-slate-800 border border-slate-600 rounded hover:bg-slate-700"
                 onClick={e => onStartLearn()}>AIが学習する</button>
